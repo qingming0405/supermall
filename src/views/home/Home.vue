50a31bb17d2d6e19254a58257618f130f76446dd
tianhe <595417564@qq.com>
2020-12-10 09:33:06 +0800

    Enter

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,15 +5,17 @@
         <div>购物街</div>
       </template>
     </nav-bar>
-    <!-- <scroll ref="scroll" class="content"> -->
+    <scroll ref="scroll" class="content" :probe-type="3" 
+            @scroll="contentScroll" 
+            :pull-up-load="true" @pulling-up="loadMore">
        <home-swiper :banners="banners"></home-swiper>
       <recommend-view :recommends="recommends"></recommend-view>
       <feature-view></feature-view>
       <tab-control class="tab-control" :titles="['流行','新款','精选']" 
                   @tabClick="tabClick"></tab-control>
       <goods-list :goods="showGoodsList"></goods-list>
-    <!-- </scroll> -->
-   
+    </scroll>
+    <back-top v-show="isShowBackUp" @click="backClick"/>
   </div>
 </template>
   
@@ -26,9 +28,10 @@ import GoodsList from 'components/content/goods/GoodsList.vue'
 import HomeSwiper from './childComps/HomeSwiper'
 import RecommendView from './childComps/RecommendView.vue'
 import FeatureView from './childComps/FeatureView.vue'
+import Scroll from 'components/common/scroll/Scroll.vue'
+import BackTop from 'components/content/backTop/BackTop.vue'
 
 import {getHomeMultidata, getHomeGoods} from 'network/home'
-// import Scroll from '../../components/common/scroll/Scroll.vue'
 
 
 export default {
@@ -40,7 +43,8 @@ export default {
     FeatureView,
     TabControl,
     GoodsList,
-    // Scroll
+    Scroll,
+    BackTop,
   },
   data() {
     return {
@@ -51,7 +55,8 @@ export default {
         'new': {page: 0, list: []},
         'sell': {page: 0, list: []}
       },
-      currentType: 'pop'
+      currentType: 'pop',
+      isShowBackUp: false
     }
   },
   computed: {
@@ -69,7 +74,6 @@ export default {
     this.getHomeGoods('sell')
   },
   methods: {
-
     /**
      * 网络请求
      */
@@ -86,6 +90,9 @@ export default {
         // console.log(res);
         this.goods[type].list.push(...res.data.list)
         this.goods[type].page += 1;
+
+        this.$refs.scroll.finishedPullUp()
+        this.$refs.scroll.refresh()
       })
     },
     /**
@@ -103,30 +110,43 @@ export default {
           this.currentType = 'sell'
           break
       }
+    },
+    backClick(){
+      this.$refs.scroll.scrollTo(0,0,500)
+    },
+    contentScroll(position){
+      this.isShowBackUp = (-position.y) > 1000
+    },
+    loadMore(){
+      this.getHomeGoods(this.currentType)
     }
   }
 }
 </script>
   
 <style scoped>
-  #home{
-    padding-top: 44px;
+  #home {
+    height: 100vh;
+    position: relative;
   }
 
-  .home-nav{
+  .home-nav {
     background-color: var(--color-tint);
     color: #fff;
-    
-    position: fixed;
-    left: 0;
-    right: 0;
-    top: 0;
+    position: relative;
     z-index: 9;
   }
 
-  .tab-control{
-    position: sticky;
-    top: 44px;
+  .tab-control {
+    position: relative;
     z-index: 9;
+  }
+
+  .content {
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
   }
 </style>
